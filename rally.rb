7917f4d72429bab7a2aa40a5d17bc7e6bb07fb27
krockode <khperkins@gmail.com>
2013-01-31 10:37:25 +1000

    gave "show" an actual subcommand and added -t show tasks

--- rally.rb
+++ rally.rb
@@ -13,7 +13,7 @@ story = nil
 c = Term::ANSIColor
 config = YAML.load_file(ENV['HOME']+'/.rallyconf.yml')['rally']
 
-SUB_COMMANDS = %w(block notes launch workon)
+SUB_COMMANDS = %w(show block notes launch workon)
 global_opts = Trollop::options do
     banner <<-EOS
 Usage: rally [options] [command [command-options]]
@@ -23,8 +23,13 @@ EOS
     stop_on SUB_COMMANDS
 end
 cmd = ARGV.shift # get the subcommand
-action = cmd && cmd.to_sym || :display
+action = cmd && cmd.to_sym || :show
 cmd_opts = case cmd
+    when "show"
+        Trollop::options do
+            banner "Block a user story"
+            opt :tasks, "display a list of the tasks for the story", :type => :bool
+        end
     when "block"
         Trollop::options do
             banner "Block a user story"
@@ -45,7 +50,7 @@ cmd_opts = case cmd
 end
 
 text = cmd_opts.message if cmd_opts
-story = global_opts.story
+story = global_opts.story.upcase()
 unless story
     # determine story id from the current git branch
     branch = IO.popen("git rev-parse --abbrev-ref HEAD") { |io|  io.first.strip }
@@ -73,15 +78,15 @@ def print_text(text)
     puts text.gsub(/\n/, "\n\n").gsub(/(.{1,#{line_width}})(\s+|$)/, "\\1\n")
 end
 
-def printable_state(rally_story)
+def printable_state(blocked, state)
     c = Term::ANSIColor
     states = {"D" => "Defined", "P" => "In-Progress", "C" => "Completed", "A" => "Accepted"}
     bg = c.on_green
     state_string = ""
     ["D", "P", "C", "A"].each do |s|
-        bg = c.on_red if (rally_story.blocked == "true" && (states[s] == rally_story.schedule_state))
+        bg = c.on_red if (blocked == "true" && (states[s] == state))
         state_string += bg + s
-        bg = c.reset if (states[s] == rally_story.schedule_state)
+        bg = c.reset if (states[s] == state)
     end
     return state_string + c.reset
 end    
@@ -98,11 +103,12 @@ def prompt_for_text
 end
 
 if story
+    story = story.upcase()
     r = connect_to_rally(config['url'], config['username'], config['password'])
     workspace = r.find_all(:workspace).first
     rally_story = r.find(:artifact, :workspace => workspace) {equal :formatted_i_d, story }.find { |s| s.formatted_i_d == story}
     type = (story.start_with?("US") && :userstory) || :defect
-    if action == :display
+    if action == :show
         id_color = c.blue
         title =  "#{rally_story.formatted_i_d}: #{rally_story.name}"
         puts id_color + c.bold(title)
@@ -114,7 +120,11 @@ if story
         end
         puts c.bold('Project: ') + rally_story.project.name
         puts c.bold('Iteration: ') + rally_story.iteration.name
-        puts c.bold('State: ') + printable_state(rally_story)
+        puts c.bold('State: ') + printable_state(rally_story.blocked, rally_story.schedule_state)
+        if cmd_opts && cmd_opts.tasks
+            puts c.bold('Tasks: ')
+            rally_story.tasks.each {|task| puts "State: #{printable_state(task.blocked, task.state)} Name: #{task.name}"}
+        end
     elsif action == :block
         blocked = rally_story.blocked == "true"
         block_state = blocked && c.cyan("unblocked") || c.red("blocked")
